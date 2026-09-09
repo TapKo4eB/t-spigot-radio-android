d71b3f57977074592908ddce660fbc25037b1ad8
TapKo4eB <tapko4eb@gmail.com>
2026-09-09 15:27:33 +0300

    some chat polishing, whatever. (v0.0.4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "net.tspigot.radio"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
-        versionName = "0.0.3"
+        versionCode = 5
+        versionName = "0.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
