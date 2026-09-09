c8e9918811af58e6e878e1887086fa8bf543b49e
TapKo4eB <tapko4eb@gmail.com>
2026-09-09 17:30:22 +0300

    allright its real device testing time (v0.0.5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "net.tspigot.radio"
         minSdk = 26
         targetSdk = 37
-        versionCode = 5
-        versionName = "0.0.4"
+        versionCode = 6
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
