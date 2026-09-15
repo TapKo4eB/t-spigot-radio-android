459476773a8bbf2496f5210e8f121b527b0980d5
TapKo4eB <tapko4eb@gmail.com>
2026-09-15 17:35:29 +0300

    i deem this be a new version (v0.0.6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "net.tspigot.radio"
         minSdk = 26
         targetSdk = 37
-        versionCode = 6
-        versionName = "0.0.5"
+        versionCode = 7
+        versionName = "0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
